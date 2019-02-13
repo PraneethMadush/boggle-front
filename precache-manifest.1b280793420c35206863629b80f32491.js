62d9677a1ed6e803a31c0d86bb6454265440a382
isuruAb <vpisabeywardana@gmail.com>
2019-02-13 11:08:00 +0800

    Updates

---
+++ precache-manifest.1b280793420c35206863629b80f32491.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "46b771992d4bc9f226aa",
+    "url": "/boggle-front/static/css/main.561e7ac5.chunk.css"
+  },
+  {
+    "revision": "46b771992d4bc9f226aa",
+    "url": "/boggle-front/static/js/main.46b77199.chunk.js"
+  },
+  {
+    "revision": "e42fd2daf0a20d22b11a",
+    "url": "/boggle-front/static/js/1.e42fd2da.chunk.js"
+  },
+  {
+    "revision": "f4280bc6b944072b5e23",
+    "url": "/boggle-front/static/js/runtime~main.f4280bc6.js"
+  },
+  {
+    "revision": "576a6ea80e68cbff382d439e55432827",
+    "url": "/boggle-front/static/media/correct-sound.576a6ea8.mp3"
+  },
+  {
+    "revision": "88417a5401c934d93de25c5c8db1fa74",
+    "url": "/boggle-front/static/media/wrong-sound.88417a54.mp3"
+  },
+  {
+    "revision": "3887037507203752cc4586744590d656",
+    "url": "/boggle-front/static/media/beep.38870375.mp3"
+  },
+  {
+    "revision": "8f346a53ee078b98deb0e78364059b95",
+    "url": "/boggle-front/static/media/not-found-bg.8f346a53.jpg"
+  },
+  {
+    "revision": "386f0e7eebc8dd647297b998ec70f315",
+    "url": "/boggle-front/index.html"
+  }
+];
